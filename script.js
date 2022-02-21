0da7bf60230bd0f4a98a967e6982d228b7c1bf3d
arafat356 <ahmedarafatft@gmail.com>
2022-02-22 02:34:46 +0600

    some

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const API_KEY = `a80c6d8564a79f344acd66bd5a17ffa4`;
 const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     //     console.log(url)
     fetch(url)
         .then(res => res.json())
         .then(data => displayTemparature(data))
 }
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text
+}
 
 const displayTemparature = temparature => {
-    console.log(temparature)
+    setInnerText('city', temparature.name);
+    setInnerText('temp-water', temparature.main.temp);
+    setInnerText('condition', temparature.weather[0].main);
+    //     setWather icon 
+    const url = `http://openweathermap.org/img/wn/${temparature.weather[0].icon}@2x.png`;
+    const ImgIcon = document.getElementById('Wather-icon');
+    ImgIcon.setAttribute('src', url);
 }
